25ebcebbf355c9fa76fb2247173c490a8f3ad973
polecoder <maurop4502@gmail.com>
2025-06-08 23:16:28 -0300

    completar Propietario.h y Cliente.h

--- include/Cliente.h
+++ include/Cliente.h
@@ -1,16 +1,83 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 #include "Usuario.h"
+#include "DTNotificacion.h"
+#include <set>
 #include <string>
 
-class Cliente : public Usuario {
-    private:
-        std::string apellido;
-        std::string documento;
+using namespace std;
 
-    public:
-        Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento);
-        ~Cliente();
+class Cliente : public Usuario
+{
+private:
+    string apellido;
+    string documento;
+    set<DTNotificacion> notificaciones;
+
+public:
+    /**
+     * @brief Constructor de Cliente
+     *
+     * @param nickname
+     * @param contrasena
+     * @param nombre
+     * @param email
+     * @param apellido
+     * @param documento
+     */
+    Cliente(string nickname, string contrasena, string nombre, string email, string apellido, string documento);
+    /**
+     * @brief Destructor de Cliente
+     *
+     */
+    ~Cliente();
+    /**
+     * @brief Getter para apellido
+     *
+     * @return Un string correspondiente a la cuenta bancaria del cliente
+     */
+    string getApellido();
+    /**
+     * @brief Getter para documento
+     *
+     * @return Un string correspondiente al documento del cliente
+     */
+    string getDocumento();
+    /**
+     * @brief Getter para notificaciones
+     *
+     * @return Un set<DTNotificacion> correspondiente a TODAS las notificaciones del cliente
+     */
+    set<DTNotificacion> getNotificaciones();
+    /**
+     * @brief Setter para apellido
+     *
+     * @param apellido Apellido para asignar al cliente
+     */
+    void setApellido(string apellido);
+    /**
+     * @brief Setter para documento
+     *
+     * @param documento Documento para asignar al cliente
+     */
+    void setDocumento(string documento);
+    /**
+     * @brief Se encarga de agregar la notificación pasada por parámetro al set de notificaciones del cliente
+     *
+     * @param notificacion La notificación que se quiere agregar al set de notificaciones
+     */
+    void agregarNotificacion(DTNotificacion const &notificacion);
+    /**
+     * @brief Se encarga de eliminar la notificación pasada por parámetro al set de notificaciones del cliente
+     *
+     * @param codigoPublicacion El código de la publicación asociada a la notificación que se quiere eliminar al set de notificaciones
+     */
+    void eliminarNotificacion(int codigoPublicacion);
+    /**
+     * @brief Se encarga de eliminar TODAS las notificaciones del set de notificaciones del cliente
+     *
+     */
+    void eliminarNotificaciones();
 };
 
 #endif
--- include/Propietario.h
+++ include/Propietario.h
@@ -1,6 +1,8 @@
 #ifndef PROPIETARIO_H
 #define PROPIETARIO_H
 #include "Usuario.h"
+#include "DTNotificacion.h"
+#include <set>
 #include <string>
 
 using namespace std;
@@ -10,9 +12,72 @@ class Propietario : public Usuario
 private:
     string cuentaBancaria;
     string telefono;
+    set<DTNotificacion> notificaciones;
 
 public:
+    /**
+     * @brief Constructor de Propietario
+     *
+     * @param nickname
+     * @param contrasena
+     * @param nombre
+     * @param email
+     * @param cuentaBancaria
+     * @param telefono
+     */
     Propietario(string nickname, string contrasena, string nombre, string email, string cuentaBancaria, string telefono);
+    /**
+     * @brief Destructor de Propietario
+     *
+     */
+    ~Propietario();
+    /**
+     * @brief Getter para cuentaBancaria
+     *
+     * @return Un string correspondiente a la cuenta bancaria del propietario
+     */
+    string getCuentaBancaria();
+    /**
+     * @brief Getter para telefono
+     *
+     * @return Un string correspondiente al telefono del propietario
+     */
+    string getTelefono();
+    /**
+     * @brief Getter para notificaciones
+     *
+     * @return Un set<DTNotificacion> correspondiente a TODAS las notificaciones del propietario
+     */
+    set<DTNotificacion> getNotificaciones();
+    /**
+     * @brief Setter para cuentaBancaria
+     *
+     * @param cuentaBancaria Cuenta bancaria para asignar al propietario
+     */
+    void setCuentaBancaria(string cuentaBancaria);
+    /**
+     * @brief Setter para telefono
+     *
+     * @param telefono Telefono para asignar al propietario
+     */
+    void setTelefono(string telefono);
+    /**
+     * @brief Se encarga de agregar la notificación pasada por parámetro al set de notificaciones del propietario
+     *
+     * @param notificacion La notificación que se quiere agregar al set de notificaciones
+     */
+    void agregarNotificacion(DTNotificacion const &notificacion);
+    /**
+     * @brief Se encarga de eliminar la notificación pasada por parámetro al set de notificaciones del propietario
+     *
+     * @param codigoPublicacion El código de la publicación asociada a la notificación que se quiere eliminar al set de notificaciones
+     */
+    void eliminarNotificacion(int codigoPublicacion);
+    /**
+     * @brief Se encarga de eliminar TODAS las notificaciones del set de notificaciones del propietario
+     *
+     */
+    void eliminarNotificaciones();
 };
 
 #endif
